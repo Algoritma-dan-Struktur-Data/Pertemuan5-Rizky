792fb16d90425d56e340215028969bb19e1e8ff7
Muhammad Rizky <rzkyyriz@gmail.com>
2026-04-10 08:31:27 +0700

    latihanp5.py

--- latihanp5.py
+++ latihanp5.py
@@ -13,19 +13,30 @@ data = [
     ]
 ]
 
-rata_mhs = [sum(mhs[1]) / len(mhs[1]) for mhs in data]
-max_avg = max(rata_mhs)
-idx_max = rata_mhs.index(max_avg)
+rata_mhs = []
 
+for mhs in data:
+    nilai = mhs[1]
+    rata = sum(nilai) / len(nilai)
+    rata_mhs.append(rata)
+
+nilai_tertinggi = max(rata_mhs)
+index_pintar = rata_mhs.index(nilai_tertinggi)
+
+jumlah_mhs = len(data)
+jumlah_mk = len(data[0][1])
 rata_mk = []
-nilai_saja = [mhs[1] for mhs in data]
 
-for nilai_per_mk in zip(*nilai_saja):
-    avg_mk = sum(nilai_per_mk) / len(nilai_per_mk)
-    rata_mk.append(avg_mk)
+for i in range(jumlah_mk):
+    total_nilai_mk = 0
+    for j in range(jumlah_mhs):
+        total_nilai_mk += data[j][1][i]
+    
+    rata_rata_mk = total_nilai_mk / jumlah_mhs
+    rata_mk.append(rata_rata_mk)
 
-min_avg_mk = min(rata_mk)
-idx_min_mk = rata_mk.index(min_avg_mk)
+nilai_mk_terendah = min(rata_mk)
+index_mk_rendah = rata_mk.index(nilai_mk_terendah)
 
-print(f"Mahasiswa Terpintar: {data[idx_max][0][1]} (Rata-rata: {round(max_avg, 2)})")
-print(f"Mata Kuliah Nilai Terkecil: MK {idx_min_mk + 1} (Rata-rata: {round(min_avg_mk, 2)})")
+print("Mahasiswa Terpintar:", data[index_pintar][0][1], "(Nilai:", round(nilai_tertinggi, 2), ")")
+print("Mata Kuliah Nilai Terkecil: MK", index_mk_rendah + 1, "(Nilai:", round(nilai_mk_terendah, 2), ")")
